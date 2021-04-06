e8e13975b4c0100ae16668a120958dde691fef67
Daniel Finimundi <daniel@finimundi.com>
2021-04-06 19:06:34 -0300

    Check if required variables are set
    Signed-off-by: Daniel Finimundi <daniel@finimundi.com>

--- push.sh
+++ push.sh
@@ -2,5 +2,10 @@
 
 set -e
 
+if [ -z "${GEMGURY_NAME}" ] || [ -z "${GEMGURY_TOKEN}" ] || [ -z "${PACKAGE_NAME}" ]; then
+    echo "Username or access token not provided, make sure you add that to project's env variables."
+    exit 1
+fi
+
 echo "Deploying package: ${PACKAGE_NAME}"
 curl -F package=@${PACKAGE_NAME} https://${GEMFURY_TOKEN}@push.fury.io/${GEMFURY_NAME}
