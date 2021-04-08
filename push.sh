367803fe879170d56a2ed0af9a773366c4e865d0
Daniel Finimundi <daniel@finimundi.com>
2021-04-08 20:17:46 -0300

    Add debug echos
    Signed-off-by: Daniel Finimundi <daniel@finimundi.com>

--- push.sh
+++ push.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+echo "GEMFURY_NAME=${GEMFURY_NAME}"
+echo "GEMFURY_TOKEN=$([ -z ${GEMFURY_TOKEN} ] || echo "[REMOVED]")"
+echo "PACKAGE_NAME=${PACKAGE_NAME}"
+
 if [ -z "${GEMFURY_NAME}" ] || [ -z "${GEMFURY_TOKEN}" ] || [ -z "${PACKAGE_NAME}" ]; then
     echo "Username or access token not provided, make sure you add that to project's env variables."
     exit 1
